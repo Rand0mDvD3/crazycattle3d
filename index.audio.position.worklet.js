760812911bacbda20e55c75e29463f414749d45d
Rand0mDvD3 <122961220+Rand0mDvD3@users.noreply.github.com>
2025-04-16 21:48:24 -0700

    Update index.audio.position.worklet.js

--- index.audio.position.worklet.js
+++ index.audio.position.worklet.js
@@ -54,12 +54,7 @@ class GodotPositionReportingProcessor extends AudioWorkletProcessor {
 			if (input.length > 0) {
 				this.position += input[0].length;
 			}
-		}
 
-		// Posting messages is expensive. Let's limit the number of posts.
-		if (currentTime - this.lastPostTime > POST_THRESHOLD_S) {
-			this.lastPostTime = currentTime;
-			this.port.postMessage({ type: 'position', data: this.position });
 		}
 
 		return true;
